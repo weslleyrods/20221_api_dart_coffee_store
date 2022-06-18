90ec8a7deeafa66de21839ce5646ef217a5b2f5c
Luan Teixeira <luanlts@outlook.com>
2022-06-17 21:55:11 -0300

    feat: implementa rotas de cliente

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 require("dotenv").config({ path: ".env" });
-
-//const Cliente = requiere('./models/cliente.js')
 const express = require("express");
 const app = express();
 const http = require("http");
 const mongoose = require("mongoose");
-const path = require("path");
 
 const {
   MONGODB_USER,
@@ -25,95 +22,20 @@ mongoose
     console.error("Erro: " + error.message);
   });
 
+// routes
+const routerCliente = require("./routes/routerCliente");
+/* const routerProduto = require("./routes/routerProduto");
+const routerCarrinho = require("./routes/routerCarrinho"); */
+
 const port = process.env.PORT || 3000;
 app.set("port", port);
 
-// models
-const Cliente = require("./models/cliente");
-const Carrinho = require("./models/carrinho");
+app.use("/cliente", routerCliente);
+/* app.use("/produto", routerProduto);
+app.use("/carrinho", routerCarrinho); */
 
-const clienteTeste = new Cliente({
-  nome: "teste",
-  sobrenome: "Silva",
-  cpf: "45678945620",
-  email: "teste@silva.com",
-  senha: "1231313",
-  telefone: "114578457845",
-  celular: "11978457845",
-  endereco: {
-    logradouro: "rua a",
-    numero: "123",
-    bairro: "vila praia",
-    cidade: "Sao paulo",
-    estado: "Sao paulo",
-    cep: "03895562",
-  },
-});
-
-/* clienteTeste
-  .save()
-  .then(() => {
-    console.log("deu bom");
-  })
-  .catch((err) => {
-    console.log(`deu ruim ${err}`);
-  }); */
-
-// app.get("/teste/imagem/:img", (req, res) => {
-//   res.sendFile(__dirname + "/image/" + req.params.img);
-// });
-
-/* //Weslley
-// o mongoose opera sobre promisses
-const id = Cliente.findOne(() => {
-  //pipe? acho que não tem pipe aqui
-  id: Cliente._id;
-}); */
-
-Cliente.findOne()
-  .then((doc) => {
-    console.log(doc);
-    console.log(`${doc._id}`);
-  })
-  .catch((err) => {
-    console.log(`deu erro ${err}`);
-  });
-
-// const carrinhoTeste = new Carrinho({
-//   valorTotal: "25,00",
-//   qtdTotal: "5",
-//   frete: "15,00",
-//   id_cliente: id,
-//   id_produto: "teste",
-// });
-
-/* carrinhoTeste
-  .save()
-  .then(() => {
-    console.log("deu bom no carrinho");
-  })
-  .catch((err) => {
-    console.log(`deu ruim no carrinho${err}`);
-  }); */
-
-/* const carrinhoTeste = new carrinho({
-  valorTotal: "25,00",
-  qtdTotal: "5",
-  frete: "15,00",
-  id_cliente: clienteTeste
-  id_produto
-
-  
-}) */
-
-/*
-  valorTotal: { type: String, required: true },
-  qtdTotal: { type: String, required: true },
-  frete: { type: String, required: true },
-  id_cliente: { type: ObjectId, required: true },
-  id_produtos: { type: [ObjectId], required: false },
-});
-*/
+// aceitar json np corpo(body) da requisicao
+app.use(express.json());
 
 const server = http.createServer(app);
 server.listen(port);
---
+++ routes/routerCliente.js
@@ -0,0 +1,141 @@
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const router = express.Router();
+
+// model
+const Cliente = require("../models/cliente");
+
+// aceitar json np corpo(body) da requisicao
+router.use(express.json());
+
+//Luan
+router.get("/:id", (req, res) => {
+  // pegar informacoes de um usuario
+  let id = req.params.id;
+  Cliente.findOne({ _id: id })
+    .then((docCliente) => {
+      res.status(200).json({
+        success: true,
+        data: docCliente,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        sucess: false,
+        message: err,
+      });
+    });
+});
+
+//Weslley
+router.post("/cadastro", (req, res) => {
+  // cadastrar um usuario
+  const cliente = new Cliente({
+    //...req.body
+    nome: req.body.nome,
+    sobrenome: req.body.sobrenome,
+    cpf: req.body.cpf,
+    email: req.body.email,
+    senha: req.body.senha,
+    telefone: req.body.telefone,
+    celular: req.body.celular,
+    endereco: [
+      {
+        logradouro: req.body.logradouro,
+        numero: req.body.numero,
+        bairro: req.body.bairro,
+        cidade: req.body.cidade,
+        estado: req.body.estado,
+        cep: req.body.cep,
+      },
+    ],
+  });
+  Cliente.findOne({ cpf: cliente.cpf }).then((doc) => {
+    if (doc === null) {
+      // retorna null se não existir cliente com o cpf
+      cliente.save().then((clienteInserido) => {
+        res.status(201).json({
+          success: true,
+          data: clienteInserido,
+        });
+      });
+    } else {
+      res.status(500).json({
+        success: false,
+        message: "O usuário já foi criado",
+      });
+    }
+  });
+});
+
+// Marcelo
+//atualizar um usuario
+router.put("/atualizar/:id", (req, res) => {
+  const filtro = { _id: req.params.id };
+
+  Cliente.updateMany(filtro, {
+    $set: {
+      nome: "",
+      sobrenome: "",
+      cpf: "",
+      email: "",
+      senha: "",
+      telefone: "",
+      celular: "",
+      endereco: [
+        {
+          logradouro: "",
+          numero: "",
+          bairro: "",
+          cidade: "",
+          estado: "",
+          cep: "",
+        },
+      ],
+    },
+  })
+    .then(() => {
+      res.status(201).json({
+        success: true,
+        message: "Dados do cliente atualizados com sucesso!",
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message:
+          "Problema na atualização dos dados do cliente, tentar novamente: " +
+          err,
+      });
+    });
+});
+
+//Rana
+//deletar um usuario
+router.delete("/remover/:id", (req, res) => {
+  Cliente.deleteOne({ _id: req.params.id })
+    .then(() => {
+      res.status(201).json({
+        success: true,
+        message: req.params.id,
+      }); //;
+      //res.redirect("/router/usuario"); //confirmar se é esse caminho mesmo  //redireciona para a URL derivada do caminho especificado
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: err,
+      }); //;
+      //res.redirect("/router/usuario"); //confirmar se é esse caminho mesmo //redireciona para a URL derivada do caminho especificado
+    });
+
+  /*
+  // delete do Bossini
+      Cliente.deleteOne({_id: req.params.id})
+      .then((resultado) => {
+          console.log(resultado)
+          res.status(200).end()    
+ */
+});
+
+module.exports = router;
